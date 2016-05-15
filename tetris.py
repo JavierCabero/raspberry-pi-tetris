356279ca61831c78a5d7f553d0ed1a872f77d3e9
JCabero <j.c.revolution@hotmail.com>
2016-05-15 18:36:54 +0200

    More pieces & Game Over message

--- tetris.py
+++ tetris.py
@@ -8,7 +8,9 @@
 
 from sense_hat import SenseHat
 import os
+import sys
 import time
+import random
 import pygame
 from pygame.locals import *
 
@@ -48,17 +50,21 @@ class Piece:
         return True
 
     def move_bottom(self, grid):
+        
         print 'CHECKING BOTTOM'
+
+        for i in range(grid.height):
+            print grid.cells[i]
+
         for pixel in self.pixels:
             if pixel[1] >= 7:
                 return False
             if not grid.isEmpty(pixel[1]+1, pixel[0]):
                 return False
             print pixel
-            print grid.cells[pixel[1]+1][pixel[0]]
 
         for pixel in self.pixels:
-            pixel[1] = pixel[1] + 1 # TODO Fix coordinate systems
+            pixel[1] = pixel[1] + 1
         return True
 
     def draw(self):
@@ -98,8 +104,23 @@ class Piece:
 
 class Triangle(Piece):
     def __init__(self):
-        self.pixels = [[3, 0], [2, 1], [3, 1], [4, 1], [5, 1]]
+        self.pixels = [[3, 0], [2, 1], [3, 1], [4, 1]]
         self.color  = [0, 0, 50]
+
+class Square(Piece):
+    def __init__(self):
+        self.pixels = [[3, 0], [4, 0], [3, 1], [4, 1]]
+        self.color  = [50, 0, 0]
+
+class RightL(Piece):
+    def __init__(self):
+        self.pixels = [[3, 0], [3, 1], [3, 2], [4, 2]]
+        self.color  = [50, 0, 0]
+
+class LeftL(Piece):
+    def __init__(self):
+        self.pixels = [[4, 0], [4, 1], [4, 2], [3, 2]]
+        self.color  = [50, 50, 0]
 
 def set_pixels(pixels, col):
     for p in pixels:
@@ -126,26 +147,34 @@ class Grid:
         for pixel in piece.pixels:
             self.cells[pixel[1]][pixel[0]] = piece.color
 
-    def check_lines(self):
+    def check_game_over(self):
 
+        for j in range(self.width):
+            if not self.isEmpty(0, j):
+                sense.show_message("Game Over")
+                sys.exit(0)
 
+    def check_full_lines(self):
 
         for i in range(self.height)[::-1]:
 
             line_complete = True
             for j in range(self.width):
-                if not self.isEmpty(i, j):
+                if self.isEmpty(i, j):
                     line_complete = False
                     break
 
             # if complete break line and move everything downwards
-            # TODO Fix
             if line_complete:
-                print 'line ' + str(i) + ' full' # TODO Fix
+                print self.cells[i]
                 for x in range(0, i+1)[::-1]:
                     self.cells[x] = self.cells[x-1]
                 self.cells[0] = [None] * self.width
-            
+
+    def check_lines(self):
+        self.check_game_over()
+        self.check_full_lines()
+
     def draw(self):
         for i in range(self.height):
             for j in range(self.width):
@@ -153,14 +182,22 @@ class Grid:
                     sense.set_pixel(j, i, self.cells[i][j])
 
 def random_piece(): 
-    return Triangle() # TODO make more pieces
+    n = random.randint(0, 3)
+    if n == 0:
+        return Triangle()
+    elif n == 1:
+        return Square()
+    elif n == 2:
+        return RightL()
+    elif n == 3:
+        return LeftL()
 
 def main():
     
     running = True
 
     global moving_piece
-    moving_piece = Triangle()
+    moving_piece = random_piece()
     moving_piece.draw()
 
     # time control
@@ -173,6 +210,8 @@ def main():
     grid = Grid(8, 8)
 
     next_piece_respawn = -1
+
+    #sense.show_message("Tetris")
 
     # main loop
     while running:
